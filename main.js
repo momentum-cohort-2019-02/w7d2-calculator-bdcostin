6e39cad8248b5318395a936888eead8e493c7fc1
bdcostin <bailey.costin@gmail.com>
2019-03-27 10:23:44 -0400

    for loop needs some work

---
+++ main.js
@@ -0,0 +1,55 @@
+`use strict`
+
+const equals = document.querySelector(`.equals`);
+const numbers = document.querySelectorAll(`.calc_keys`);
+const arithmeticOperators = document.querySelectorAll(`.operators`);
+const ampersand = document.querySelector(`.ampersand`);
+const clear = document.querySelector(`.clear`);
+const display = document.getElementById(`.display`);
+
+function doMath(input) {
+    return eval(input);
+}
+
+function multiplication(ampersand) {
+    switch (ampersand) {
+        case `*`:
+    }
+}
+
+function calculator() {
+    let input = ``;
+    let calculation = ``;
+    for (let button in numbers) {
+        button.addEventListener('click', function () {
+            display.innerText = button.innerText;
+            calculation += input;
+        });
+        for (let button in arithmeticOperators) {
+            button.addEventListener('click', function () {
+                display.innerText = button.innerText;
+                calculation += input;
+            });
+        }
+
+        clear.addEventListener('click', function () {
+            display.innerText = ``;
+            calculation = ``;
+            calculatedText = ``;
+        });
+
+        equals.addEventListener('click', function () {
+            if (evalText != ``) {
+                display.calculation = doMath(calculation);
+                display.innerText = calculation;
+                calculatedText = calculation;
+            }
+        });
+    }
+}
+
+function main() {
+    calculator();
+}
+
+document.addEventListener('DOMContentLoaded', main)
